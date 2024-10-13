6146ce4446b601faa3e3285b8602b7f4abe079b4
Fedecardozo <Fedecardozo.a@gmail.com>
2024-10-12 21:30:00 -0300

    mas modificaciones

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,10 +13,6 @@ import {
   IonIcon,
 } from '@ionic/angular/standalone';
 import { UsersService } from '../services/user.service';
-import { UtilsService } from '../services/utils.service';
-import { FirebaseService } from '../services/firebase.service';
-import { Imagen } from '../modals/imagen';
-import { Alert } from '../modals/alert';
 
 @Component({
   selector: 'app-home',
@@ -41,93 +37,6 @@ export class HomePage {
   constructor() {}
   public auth: UsersService = inject(UsersService);
   public router: Router = inject(Router);
-  imgUrl: string | undefined = '';
-  utils: UtilsService = inject(UtilsService);
-  userService: UsersService = inject(UsersService);
-  fire: FirebaseService = inject(FirebaseService);
-  ocultarHome: boolean = false;
-
-  //Cuando termine borrar esta linea onit
-  // ngOnInit(): void {
-  //   this.router.navigateByUrl('/cosas-lindas');
-  // }
-
-  //Tomar o seleccionar una imagen
-  async takeImage() {
-    this.imgUrl = (await this.utils.takePicture()).dataUrl;
-  }
-
-  subirCosasLindas() {
-    this.takeImage().then(async () => {
-      const idUrl: number = Date.now();
-      const path: string = `/CosasLindas/${idUrl}`;
-
-      if (await this.subirDatabaseStorage(path, idUrl, 'imagenes_lindas')) {
-        Alert.bien('Se subio correctamente', '').then((res) => {
-          if (res.isConfirmed) {
-            this.router.navigateByUrl('/cosas-lindas');
-            this.ocultarHome = false;
-          }
-        });
-      } else {
-        Alert.error(
-          'Intentelo nuevamente',
-          'hubo un error al cargar la imagen'
-        ).then((res) => {
-          if (res.isConfirmed) this.ocultarHome = false;
-        });
-      }
-    });
-  }
-
-  subirCosasFeas() {
-    this.takeImage().then(async () => {
-      const idUrl: number = Date.now();
-      const path: string = `/CosasFeas/${idUrl}`;
-
-      if (await this.subirDatabaseStorage(path, idUrl, 'imagenes_feas')) {
-        Alert.bien('Se subio correctamente', '').then((res) => {
-          if (res.isConfirmed) {
-            this.router.navigateByUrl('/cosas-feas');
-            this.ocultarHome = false;
-          }
-        });
-      } else {
-        Alert.error(
-          'Intentelo nuevamente',
-          'hubo un error al cargar la imagen'
-        ).then((res) => {
-          if (res.isConfirmed) this.ocultarHome = false;
-        });
-      }
-    });
-  }
-
-  async subirDatabaseStorage(
-    path: string,
-    idUrl: number,
-    collectioName: string
-  ): Promise<boolean> {
-    let retorno: boolean = false;
-    this.utils.mostrarSpinner('Subiendo imagen...');
-    this.ocultarHome = true;
-
-    if (this.imgUrl) {
-      await this.fire.uploadImage(path, this.imgUrl).then(async (res) => {
-        if (res && this.userService.correo && this.imgUrl) {
-          const imagen: Imagen = new Imagen(
-            this.userService.correo,
-            idUrl,
-            this.imgUrl
-          );
-          await this.fire.agregarImagenDb(imagen, collectioName);
-          retorno = imagen ? true : false;
-          this.utils.ocultarSpinner();
-        }
-      });
-    }
-    return retorno;
-  }
 
   async cerrarSesion() {
     await this.auth.cerrarSesion();
--- src/app/modals/alert.ts
+++ src/app/modals/alert.ts
@@ -6,8 +6,10 @@ export class Alert {
     backdrop: true, // Esta opción asegura que el fondo sea oscuro
     allowOutsideClick: false, // Impide cerrar al hacer clic fuera del modal
     confirmButtonText: 'Aceptar',
+    showConfirmButton: false,
     background: '#310a6b',
     color: '#f5f2ff',
+    timer: 2100,
     customClass: {
       confirmButton: 'btn-ok',
       title: 'alert-title',
